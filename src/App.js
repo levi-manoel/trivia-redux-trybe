97d078bfaac2e9066e0415d939a9b5f64445d11d
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-21 20:12:25 +0100

    Config Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Game from './pages/Game';
 import logo from './trivia.png';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" render={ () => <Login /> } />
+        <Route path="/settings" render={ () => <Settings /> } />
+        <Route path="/game" render={ () => <Game /> } />
+      </Switch>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" height="200" />
+          <p>
+            SUA VEZ
+          </p>
+        </header>
+      </div>
+    </>
   );
 }
