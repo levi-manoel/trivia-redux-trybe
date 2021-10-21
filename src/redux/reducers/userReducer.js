24bd8c6ecfe56c6f50d64089ed5a5fbf4b149af0
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-21 19:57:58 +0100

    Config Redux

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  email: '',
+  name: '',
+  token: '',
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
