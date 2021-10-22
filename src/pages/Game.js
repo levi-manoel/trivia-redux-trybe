a81c0c18cdc263abb6503bebfa45e74de07b5a73
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-10-22 19:11:48 +0100

    iniciando o req 5

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import getTrivia from '../services/getTriviaApi';
 
 export default class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+    };
+  }
+
+  async componentDidMount() {
+    await getTrivia();
+  }
+
   render() {
+    const { questions } = this.state;
     return (
-      <div>Game</div>
+      <main>
+        <h1>Game</h1>
+        <section>
+          <span>{ questions }</span>
+        </section>
+      </main>
     );
   }
 }
