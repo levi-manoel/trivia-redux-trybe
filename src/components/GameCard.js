a24d7c277c218b4186e767118b2aad14823653eb
ronaldosenajr <ronaldosenajr@gmail.com>
2021-10-23 22:26:47 -0400

    Finalizado o requisito 7

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class GameCard extends Component {
+  constructor() {
+    super();
+    this.state = { alternatives: [] };
+  }
+
+  componentDidMount() {
+    const { handleCanClick } = this.props;
+    handleCanClick(true);
+    this.randomAlternative();
+  }
+
   randomAlternative = () => {
-    const { wrongAlternative } = this.props;
+    const { wrongAlternative, correct } = this.props;
     const array = [...wrongAlternative];
     let currentIndex = wrongAlternative.length;
     let randomIndex = 0;
@@ -17,11 +28,65 @@ class GameCard extends Component {
         array[randomIndex],
       ] = [array[randomIndex], array[currentIndex]];
     }
-    return array;
+
+    const alternatives = this.createAlternativesButtons(array, correct);
+    this.setState({ alternatives });
+  }
+
+  createAlternativesButtons = (wrongAlternatives, correctAlternative) => {
+    const wrong = wrongAlternatives
+      .map((alternative, index) => (
+        <button
+          key={ alternative }
+          type="button"
+          data-testid={ `wrong-answer-${index}` }
+          onClick={ this.handleClick }
+          className="buttons"
+        >
+          {alternative}
+        </button>
+      ));
+    const correct = (
+      <button
+        type="button"
+        name="correct"
+        data-testid="correct-answer"
+        onClick={ this.handleClick }
+        key={ correctAlternative }
+        className="buttons"
+      >
+        {correctAlternative}
+      </button>);
+    return [...wrong, correct];
+  }
+
+  changeBackgroudColor = async () => {
+    const buttons = document.getElementsByClassName('buttons');
+    for (let index = 0; index < buttons.length; index += 1) {
+      const element = buttons[index];
+      if (element.name === 'correct') {
+        element.style.border = '3px solid rgb(6, 240, 15)';
+      } else {
+        element.style.border = '3px solid rgb(255, 0, 0)';
+      }
+    }
+  }
+
+  handleClick = ({ target }) => {
+    const { canClick, handleCanClick } = this.props;
+
+    if (target.name === 'correct' && canClick) {
+      this.changeBackgroudColor();
+    } else if (canClick) {
+      console.log('incorreto');
+      this.changeBackgroudColor();
+    }
+    handleCanClick(false);
   }
 
   render() {
-    const { category, question, correct } = this.props;
+    const { category, question } = this.props;
+    const { alternatives } = this.state;
     return (
       <section>
         <h3 data-testid="question-category">{category}</h3>
@@ -30,16 +95,9 @@ class GameCard extends Component {
         >
           {question.replace(/&quot;/g, '"').replace(/&#039;/g, /'/)}
         </h3>
-        <button type="button" data-testid="correct-answer">{correct}</button>
-        {this.randomAlternative().map((alternative, index) => (
-          <button
-            key={ alternative }
-            type="button"
-            data-testid={ `wrong-answer-${index}` }
-          >
-            {alternative}
-          </button>
-        ))}
+        {
+          alternatives
+        }
       </section>
     );
   }
