865bc6372cc8795b28b4101bf069706b7f9fe3e3
Eduardomak <eduardomak92@gmail.com>
2021-10-25 18:04:08 -0300

    requisito 12

---
+++ src/pages/Feedback.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        Feedback
+      </div>
+    );
+  }
+}
+export default Feedback;
