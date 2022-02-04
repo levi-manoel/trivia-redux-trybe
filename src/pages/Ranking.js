4a192bfc22495b10eddfc9ebb3e2414ebde81a5f
levi-manoel <levimanoel.deob@gmail.com>
2022-02-03 22:09:57 -0300

    "finaliza" projeto para o publicar

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking 3000</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking 3000</h1>
+        <h1>EM CONSTRUÇÃO</h1>
+        <Link to="/">
+          <button
+            type="button"
+          >
+            Volte ao começo
+          </button>
+        </Link>
+      </>
     );
   }
 }
